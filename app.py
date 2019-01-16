9966a0fac2a852870c80952f625516862384e95d
Nithin <nithingif@gmailc.om>
2019-01-16 13:56:59 +0530

    app settings

--- app.py
+++ app.py
@@ -1,7 +1,9 @@
 import os
 
 from flask import Flask
+
 app = Flask(__name__)
+app.config.from_object(os.environ['APP_SETTINGS'])
 
 
 @app.route('/')
@@ -12,6 +14,7 @@ def hello():
 @app.route('/<name>')
 def hello_name(name):
     return "Hello {}!".format(name)
+
 
 if __name__ == '__main__':
     print(os.environ['APP_SETTINGS'])
